19543d855e00774c32209bd63491cba8c49fdde9
poppychan21 <knguyen19509@gmail.com>
2024-04-27 22:39:50 -0700

    2nd progress so far :')
    added navigation and 3 other activities

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,29 +11,43 @@ import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 
-
 public class MainActivity extends AppCompatActivity {
-    private Button move;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        */
 
-        move=findViewById(R.id.donator_btn);
-        move.setOnClickListener(new View.OnClickListener() {
+        /*Initializing buttons*/
+        // for donator register button
+        Button donatorRegisterButton = findViewById(R.id.donator_register_btn);
+        // for donator sign up button
+        Button donatorSignUpButton = findViewById(R.id.donator_signin_btn);
+        // for volunteer sign up button
+        Button volunteerSignUpButton = findViewById(R.id.volunteer_signin_btn);
+
+        // These sets click listener for each button
+        donatorRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Donator_page.class);
-                startActivity(intent);
+                Intent intent1 = new Intent(MainActivity.this, Donator_SignUp.class);
+                startActivity(intent1);
+            }
+        });
+
+        donatorSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent intent2 = new Intent(MainActivity.this, Donator_Register.class);
+            startActivity(intent2);
+            }
+        });
+
+        volunteerSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(MainActivity.this, Volunteer_SignUp.class);
+                startActivity(intent3);
             }
         });
     }
